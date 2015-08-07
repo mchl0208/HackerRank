96d3e2a4374bc79f158c206af10bad4eae6aee10
unknown <mmrosario@indra-r9n5xmz.indra-rd>
2015-08-07 10:56:53 -0400

    Pangrams problem
    (20/20)

---
+++ Pangrams/Pangrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine().ToLower();
+            var strinArray = Regex.Replace(x, @"\s+", "").ToCharArray().Distinct();
+            int counter = 0;
+
+            foreach (var caracter in strinArray)
+            {
+                counter += caracter;
+            }
+
+            if (counter == 2847) Console.WriteLine("pangram");
+            else Console.WriteLine("not pangram");
+
+        }
+    }
+}
