ffc6b8ed940a5664d53974e5dcad08cf390d7a55
unknown <mmrosario@indra-r9n5xmz.indra-rd>
2015-08-12 17:40:45 -0400

    Lonely Integer Problem
    20/20

---
+++ Lonely Integer/Lonely Integer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Lonely_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            var a = Console.ReadLine();
+            var aint = a.Split(' ').Select(nu => Convert.ToInt32(nu)).ToArray();
+
+            Array.Sort(aint, (i1, i2) => i1.CompareTo(i2));
+
+            if (n == 1)
+                Console.WriteLine(aint[0]);
+            else
+            {
+                for (var c = 1; c <= n - 1; c += 2)
+                {
+                    if (aint[c] - aint[c - 1] != 0)
+                    {
+                        if (c == 1 || c % 2 != 0)
+                            Console.WriteLine(aint[c - 1]);
+                        else if (c % 2 == 0)
+                            Console.WriteLine(aint[c]);
+                        return;
+                    }
+                }
+                Console.WriteLine(aint[n - 1]);
+            }
+        }
+    }
+}
