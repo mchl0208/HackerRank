4ac4cf0fb81458bad640a900dd35cc297b38660c
unknown <mmrosario@indra-r9n5xmz.indra-rd>
2015-08-13 15:56:28 -0400

    Insertion Sort - Part 1 problem
    20/20

---
+++ Insertion Sort - Part 1/Insertion Sort - Part 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Insertion_Sort___Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Convert.ToInt32(Console.ReadLine());
+            var a = Console.ReadLine();
+            var ar = a.Split(' ').Select(nu => Convert.ToInt32(nu)).ToArray();
+
+            insertionSort(ar, ar[s - 1], ar.Length - 1);
+        }
+
+        static void insertionSort(int[] ar, int unsorted, int insertPosition)
+        {
+            if (insertPosition == 0 || ar[insertPosition - 1] < unsorted)
+            {
+                ar[insertPosition] = unsorted;
+                Console.WriteLine(string.Join(" ", ar));
+            }
+            else
+            {
+                ar[insertPosition] = ar[insertPosition - 1];
+                Console.WriteLine(string.Join(" ", ar));
+                insertionSort(ar, unsorted, --insertPosition);
+            }
+            
+        }
+    }
+}
