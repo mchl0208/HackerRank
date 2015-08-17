ccdda265902b6ef0128a7f6379e0a11706e3319e
unknown <mmrosario@indra-r9n5xmz.indra-rd>
2015-08-17 15:26:12 -0400

    Fibonacci Modified problem
    15/20 (Bad use of BigInteger and iterations) Must solve.

---
+++ Fibonacci Modified/Fibonacci Modified/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Fibonacci_Modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lenum = new BigInteger();
+            var a = Console.ReadLine().Split(' ').Select(nu => Convert.ToInt32(nu)).ToArray();
+
+            BigInteger A = a[0];
+            BigInteger B = a[1];
+
+            for (int c = 0; c < a[2] - 2; c++)
+            {
+                lenum = (A + (B * B));
+                A = B;
+                B = lenum;
+            }
+
+            Console.WriteLine(lenum);
+            Console.ReadKey();
+
+        }
+    }
+}
